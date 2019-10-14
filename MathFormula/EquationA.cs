3fe96cafdeac656a65cc458316fd3c870b2b303c
s198061 <s198061@LAF-7E868C>
2019-10-14 15:12:02 -0500

    Math formalor A-D

---
+++ MathFormula/EquationA.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathFormula
+{
+    class EquationA
+    {
+        public double MathA(float x)
+        {
+            double reresult = x * 2;
+            double result = Math.Pow(x, 2) + reresult - 7;
+            return result;
+        }
+
+        public double MathBA(float a, float b, float c)
+        {
+            double result = -b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+            
+            double Result = result / 2 * a;
+            
+            return Result;
+        }
+
+        public double MathBB(float a, float b, float c)
+        {
+            double result = -b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c);
+            double Result = result / 2 * a;
+            return Result;
+        }
+
+        public double MathC(float s, float e, float t)
+        {
+            double result = e - s;
+            double Result = s + t * result;
+            return Result;
+        }
+
+        public double MathD(float x1, float x2, float y1, float y2)
+        {
+            double result = x2 - x1;
+            double reresult = y2 - y1;
+            double Result = Math.Sqrt(Math.Pow(result, 2) + Math.Pow(reresult, 2));
+            return Result;
+        }
+
+        public double MathE()
+        {
+
+        }
+    }
+}
--- MathFormula/Program.cs
+++ MathFormula/Program.cs
@@ -10,6 +10,29 @@ namespace MathFormula
     {
         static void Main(string[] args)
         {
+            float x = 0.00f;
+            float a = 0.00f;
+            float b = 0.00f;
+            float c = 0.00f;
+            float s = 0.00f;
+            float e = 0.00f;
+            float t = 0.00f;
+            float x1 = 0.00f;
+            float x2 = 0.00f;
+            float y1 = 0.00f;
+            float y2 = 0.00f;
+
+            EquationA Meth = new EquationA();
+
+            Console.WriteLine(Meth.MathA(x));
+            Console.WriteLine(Meth.MathBA(a, b, c));
+            Console.WriteLine(Meth.MathBB(a, b, c));
+            Console.WriteLine(Meth.MathC(s, e, t));
+            Console.WriteLine(Meth.MathD(x1, x2, y1, y2));
+
+            Console.ReadLine();
         }
+        
+
     }
 }
